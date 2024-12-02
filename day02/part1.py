396c6bb21c8264f6bec987c20bbbbbc539fb5b0a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-12-03 00:28:25 +0100

    refactor

--- day02/part1.py
+++ day02/part1.py
@@ -1,7 +1,7 @@
-from utils import convert_list_to_int
-from utils import list_is_increasing
-from utils import list_is_decreasing
-from utils import list_has_no_further_distance_than
+from utils import list_convert_to_int
+from utils import is_increasing_list
+from utils import is_decreasing_list
+from utils import has_no_further_distance_than
 
 file = open("input.txt")
 input = file.read().splitlines()
@@ -9,9 +9,9 @@ file.close()
 
 safe_reports = 0
 for line in input:
-    report = convert_list_to_int(line.split())
-    if (list_is_decreasing(report) or list_is_increasing(report)) \
-        and list_has_no_further_distance_than(3, report):
+    report = list_convert_to_int(line.split())
+    if (is_decreasing_list(report) or is_increasing_list(report)) \
+        and has_no_further_distance_than(3, report):
         safe_reports += 1
 
 print(f"Total safe reports: {safe_reports}")
--- day02/utils.py
+++ day02/utils.py
@@ -1,11 +1,11 @@
 #To convert a list of strings to a list of ints
-def convert_list_to_int(list):
+def list_convert_to_int(list):
     for i in range(len(list)):
         list[i] = int(list[i])
     return list
 
 #Checks if list is increasing
-def	list_is_increasing(list):
+def	is_increasing_list(list):
     for i in range(len(list) - 1):
         is_increasing = list[i] < list[i + 1]
         if not is_increasing:
@@ -13,7 +13,7 @@ def	list_is_increasing(list):
     return True
 
 #Checks if list is decreasing
-def	list_is_decreasing(list):
+def	is_decreasing_list(list):
     for i in range(len(list) - 1):
         is_decreasing = list[i] > list[i + 1]
         if not is_decreasing:
@@ -21,7 +21,7 @@ def	list_is_decreasing(list):
     return True
 
 #Checks if list is decreasing
-def	list_has_no_further_distance_than(num, list):
+def	has_no_further_distance_than(num, list):
     for i in range(len(list) - 1):
         smaller_than_num = abs(list[i] - list[i + 1]) <= num
         if not smaller_than_num:
