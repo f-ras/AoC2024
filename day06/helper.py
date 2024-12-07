fe763e942bec8fb87f0e69538e6cf9f829f5aa09
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-12-07 14:28:51 +0100

    finished part 2 (todo: cleanup)

--- day06/helper.py
+++ day06/helper.py
@@ -4,6 +4,7 @@ from utils import insert_in_str_at
 GUARD = '^'
 WALL = '#'
 PATH = 'X'
+REPEATING_PATH = 'O'
 
 X = 0
 Y = 1
@@ -24,14 +25,14 @@ def	move_guard(input : list, GuardPosition : list, direction : str) -> int:
 	moves = 0
 	while not is_guard_end_of_map(input, GuardPosition)\
 		and input[GuardPosition[Y] + y_move][GuardPosition[X] + x_move] != WALL:
-		input[GuardPosition[Y]] = insert_in_str_at(GuardPosition[X], input[GuardPosition[Y]], 'X')
+		input[GuardPosition[Y]] = insert_in_str_at(GuardPosition[X], input[GuardPosition[Y]], PATH)
 		GuardPosition[Y] += y_move
 		GuardPosition[X] += x_move
 		if not input[GuardPosition[Y]][GuardPosition[X]] == PATH:
 			moves += 1
 	return moves
 
-#Not interfering with self - no path markings
+#Checking for path repeating
 def	move_guard_pt2(input : list, GuardPosition : list, direction : str):
 	x_move = 0
 	y_move = 0
@@ -46,10 +47,16 @@ def	move_guard_pt2(input : list, GuardPosition : list, direction : str):
 	else:
 		return 0
 	while not is_guard_end_of_map(input, GuardPosition)\
-		and not input[GuardPosition[Y] + y_move][GuardPosition[X] + x_move] != GUARD\
 		and input[GuardPosition[Y] + y_move][GuardPosition[X] + x_move] != WALL:
+		if input[GuardPosition[Y]][GuardPosition[X]] == PATH:
+			input[GuardPosition[Y]] = insert_in_str_at(GuardPosition[X], input[GuardPosition[Y]], REPEATING_PATH)
+		if not input[GuardPosition[Y]][GuardPosition[X]] == REPEATING_PATH:
+			input[GuardPosition[Y]] = insert_in_str_at(GuardPosition[X], input[GuardPosition[Y]], PATH)
 		GuardPosition[Y] += y_move
 		GuardPosition[X] += x_move
+		# for line in input:
+		# 	print(line)
+		# time.sleep(0.2)
 
 def get_guard_position(input : list) -> list:
 	for y in range(len(input)):
---
+++ day06/part2.py
@@ -0,0 +1,56 @@
+from helper import get_guard_position
+from helper import is_guard_end_of_map
+from helper import move_guard_pt2
+from utils import insert_in_str_at
+import copy
+
+# Constants for map symbols
+GUARD = '^'
+WALL = '#'
+PATH = 'X'
+REPEATING_PATH = 'O'
+
+X = 0
+Y = 1
+# Read input from file
+with open("input.txt") as file:
+    input = file.read().splitlines()
+
+import time
+# Move the guard until it reaches the end of the map
+def is_guard_running_around_in_loops(input : list) -> bool:
+	directions = ["NORTH", "EAST", "SOUTH", "WEST"]
+	path_repeats = 0
+	while not is_guard_end_of_map(input, GuardPosition):
+		for direction in directions:
+			move_guard_pt2(input, GuardPosition, direction)
+			if input[GuardPosition[Y]][GuardPosition[X]] == REPEATING_PATH:
+				path_repeats += 1
+				# print(f"repeat{path_repeats}")
+				# time.sleep(1)
+			else:
+				path_repeats = 0
+				# print(f"reset_repeat{path_repeats}")
+				# time.sleep(1)
+			if path_repeats >= 4:
+				return True
+	return False
+
+import time
+loops = 0
+for y in range(len(input)):
+	for x in range(len(input[y])):
+		GuardPosition = get_guard_position(input)
+		if GuardPosition[X] == x and GuardPosition[Y] == y:
+			continue
+		temp = input.copy()
+		temp[y] = insert_in_str_at(x, temp[y], WALL)
+		loops += is_guard_running_around_in_loops(temp)
+		for line in temp:
+			print(line)
+		print(loops)
+		print('\n')
+		# time.sleep(0.1)
+
+print(f"Final position {GuardPosition}")
+# print(loops)
